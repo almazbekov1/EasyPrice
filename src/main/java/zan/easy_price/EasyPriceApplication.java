1c46eadbe07de87204e79f4ff72858671f388c20
Aidar <03almabek0v@gmail.com>
2021-11-19 17:08:23 +0600

    first commit

---
+++ src/main/java/zan/easy_price/EasyPriceApplication.java
@@ -0,0 +1,13 @@
+package zan.easy_price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyPriceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zan/easy_price/EasyPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package zan.easy_price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
